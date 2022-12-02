04c9879f440a27cde130424efea8c8db7e15c243
ADUB <rootbeer608@gmail.com>
2022-12-02 14:43:46 -0800

    universal styling

--- app/forms/station_forms.py
+++ app/forms/station_forms.py
@@ -16,17 +16,22 @@ class StationForm(FlaskForm):
     name = StringField('Station Name', validators=[
                        DataRequired(), Length(max=100), station_name_exists])
     admin_id = IntegerField('Admin Id', validators=[DataRequired()])
-    stream_url = StringField('Stream URL', validators=[DataRequired(), URL()])
-    image_url = StringField('Image URL', validators=[DataRequired(), URL()])
-    chat_url = StringField('Chat URL', validators=[Optional(), URL()])
-    website_url = StringField('Website URL', validators=[Optional(), URL()])
-    calendar_url = StringField('Calendar URL', validators=[Optional(), URL()])
+    stream_url = StringField('Stream URL', validators=[
+                             DataRequired(), URL(message="Please Enter A Valid URL")])
+    image_url = StringField('Image URL', validators=[
+                            DataRequired(), URL(message="Please Enter A Valid URL")])
+    chat_url = StringField('Chat URL', validators=[
+                           Optional(), URL(message="Please Enter A Valid URL")])
+    website_url = StringField('Website URL', validators=[
+                              Optional(), URL(message="Please Enter A Valid URL")])
+    calendar_url = StringField('Calendar URL', validators=[
+                               Optional(), URL(message="Please Enter A Valid URL")])
     additional_link_1 = StringField(
-        'Additional Link 1', validators=[Optional(), URL()])
+        'Additional Link 1', validators=[Optional(), URL(message="Please Enter A Valid URL")])
     additional_link_2 = StringField(
-        'Additional Link 2', validators=[Optional(), URL()])
+        'Additional Link 2', validators=[Optional(), URL(message="Please Enter A Valid URL")])
     additional_link_3 = StringField(
-        'Additional Link 3', validators=[Optional(), URL()])
+        'Additional Link 3', validators=[Optional(), URL(message="Please Enter A Valid URL")])
     additional_label_1 = StringField('Additional Label 1', validators=[
         Length(max=50)])
     additional_label_2 = StringField('Additional Label 2', validators=[
@@ -34,7 +39,7 @@ class StationForm(FlaskForm):
     additional_label_3 = StringField('Additional Label 3', validators=[
         Length(max=50)])
     now_playing_url = StringField(
-        'Now Playing URL', validators=[Optional(), URL()])
+        'Now Playing URL', validators=[Optional(), URL(message="Please Enter A Valid URL")])
 
 
 class EditStationForm(FlaskForm):
@@ -42,16 +47,20 @@ class EditStationForm(FlaskForm):
                        DataRequired(), Length(max=100)])
     admin_id = IntegerField('Admin Id', validators=[DataRequired()])
     stream_url = StringField('Stream URL', validators=[DataRequired()])
-    image_url = StringField('Image URL', validators=[DataRequired(), URL()])
-    chat_url = StringField('Chat URL', validators=[Optional(), URL()])
-    website_url = StringField('Website URL', validators=[Optional(), URL()])
-    calendar_url = StringField('Calendar URL', validators=[Optional(), URL()])
+    image_url = StringField('Image URL', validators=[
+                            DataRequired(), URL(message="Please Enter A Valid URL")])
+    chat_url = StringField('Chat URL', validators=[
+                           Optional(), URL(message="Please Enter A Valid URL")])
+    website_url = StringField('Website URL', validators=[
+                              Optional(), URL(message="Please Enter A Valid URL")])
+    calendar_url = StringField('Calendar URL', validators=[
+                               Optional(), URL(message="Please Enter A Valid URL")])
     additional_link_1 = StringField(
-        'Additional Link 1', validators=[Optional(), URL()])
+        'Additional Link 1', validators=[Optional(), URL(message="Please Enter A Valid URL")])
     additional_link_2 = StringField(
-        'Additional Link 2', validators=[Optional(), URL()])
+        'Additional Link 2', validators=[Optional(), URL(message="Please Enter A Valid URL")])
     additional_link_3 = StringField(
-        'Additional Link 3', validators=[Optional(), URL()])
+        'Additional Link 3', validators=[Optional(), URL(message="Please Enter A Valid URL")])
     additional_label_1 = StringField('Additional Label 1', validators=[
         Length(max=50)])
     additional_label_2 = StringField('Additional Label 2', validators=[
@@ -59,4 +68,4 @@ class EditStationForm(FlaskForm):
     additional_label_3 = StringField('Additional Label 3', validators=[
         Length(max=50)])
     now_playing_url = StringField(
-        'Now Playing URL', validators=[Optional(), URL()])
+        'Now Playing URL', validators=[Optional(), URL(message="Please Enter A Valid URL")])
--- app/seeds/radio_stations.py
+++ app/seeds/radio_stations.py
@@ -4,19 +4,97 @@ from app.models import db, User, RadioStation, environment, SCHEMA
 # Adds a demo user, you can add other users here if you want
 def seed_radio_stations():
     kiosk = RadioStation(
-        name='Kiosk Radio', admin_id=1, stream_url="https://kioskradiobxl.out.airtime.pro/kioskradiobxl_b", image_url="https://yt3.ggpht.com/ytc/AMLnZu_yB69zZxUW9guNKoN6WG5_URTPZRsmTIlfGRr_ug=s900-c-k-c0x00ffffff-no-rj", website_url="https://kioskradio.com/", now_playing_url="https://kioskradiobxl.airtime.pro/api/live-info-v2")
+        name='Kiosk Radio',
+        admin_id=2,
+        stream_url="https://kioskradiobxl.out.airtime.pro/kioskradiobxl_b",
+        image_url="https://yt3.ggpht.com/ytc/AMLnZu_yB69zZxUW9guNKoN6WG5_URTPZRsmTIlfGRr_ug=s900-c-k-c0x00ffffff-no-rj", website_url="https://kioskradio.com/",
+        chat_url="",
+        now_playing_url="https://kioskradiobxl.airtime.pro/api/live-info-v2",
+        calendar_url="",
+        additional_link_1="",
+        additional_label_1="",
+        additional_link_2="",
+        additional_label_2="",
+        additional_link_3="",
+        additional_label_3="",)
     ifmdisco = RadioStation(
-        name='Intergalactic FM: Disco Fetish', admin_id=1, stream_url="https://radio.intergalactic.fm/2", image_url="https://cdn-profiles.tunein.com/s99619/images/logog.png?t=152604", website_url="https://www.intergalactic.fm/channels/disco-fetish", now_playing_url="https://www.intergalactic.fm/now-playing?channel=Disco%20Fetish")
+        name='Intergalactic FM: Disco Fetish', admin_id=2,
+        stream_url="https://radio.intergalactic.fm/2",
+        image_url="https://cdn-profiles.tunein.com/s99619/images/logog.png?t=152604",
+        website_url="https://www.intergalactic.fm/channels/disco-fetish",
+        chat_url="",
+        now_playing_url="https://www.intergalactic.fm/now-playing?channel=Disco%20Fetish",
+        calendar_url="",
+        additional_link_1="",
+        additional_label_1="",
+        additional_link_2="",
+        additional_label_2="",
+        additional_link_3="",
+        additional_label_3="",)
     ifmcbs = RadioStation(
-        name='Intergalactic FM: Cybernetic BroadCasting System', admin_id=1, stream_url="https://radio.intergalactic.fm/1", image_url="https://cdn-profiles.tunein.com/s99619/images/logog.png?t=152604", website_url="https://static-media.streema.com/media/cache/05/ea/05eafaaa52fe52cc9f984b1989432b7c.jpg", now_playing_url="https://www.intergalactic.fm/now-playing?channel=Cybernetic%20Broadcasting%20System")
+        name='Intergalactic FM: Cybernetic BroadCasting System',
+        admin_id=1,
+        stream_url="https://radio.intergalactic.fm/1",
+        image_url="https://cdn-profiles.tunein.com/s99619/images/logog.png?t=152604",
+        website_url="https://static-media.streema.com/media/cache/05/ea/05eafaaa52fe52cc9f984b1989432b7c.jpg",
+        chat_url="",
+        now_playing_url="https://www.intergalactic.fm/now-playing?channel=Cybernetic%20Broadcasting%20System",
+        calendar_url="",
+        additional_link_1="",
+        additional_label_1="",
+        additional_link_2="",
+        additional_label_2="",
+        additional_link_3="",
+        additional_label_3="",)
     refuge = RadioStation(
-        name='Refuge WorldWide', admin_id=1, stream_url="https://streaming.radio.co/s3699c5e49/listen", image_url="https://refugeworldwide.com/og-thumb.jpg", website_url="https://refugeworldwide.com/", chat_url="https://refugeworldwide.com/chat", now_playing_url="https://public.radio.co/stations/s3699c5e49/status")
+        name='Refuge WorldWide',
+        admin_id=2,
+        stream_url="https://streaming.radio.co/s3699c5e49/listen",
+        image_url="https://refugeworldwide.com/og-thumb.jpg",
+        website_url="https://refugeworldwide.com/",
+        chat_url="https://refugeworldwide.com/chat",
+        now_playing_url="https://public.radio.co/stations/s3699c5e49/status",
+        calendar_url="",
+        additional_link_1="",
+        additional_label_1="",
+        additional_link_2="",
+        additional_label_2="",
+        additional_link_3="",
+        additional_label_3="",)
 
     particle = RadioStation(
-        name='Particle FM', admin_id=2, stream_url="https://azuracast.particle.fm/radio/8000/radio.mp3", image_url="https://i1.sndcdn.com/avatars-oEn7XKE76IZpyoKB-ANzgWg-t500x500.jpg", website_url="https://particle.fm/", chat_url="https://particle.fm/chat.html", now_playing_url="https://azuracast.particle.fm/api/nowplaying/1")
+        name='Particle FM',
+        admin_id=2,
+        stream_url="https://azuracast.particle.fm/radio/8000/radio.mp3",
+        image_url="https://i1.sndcdn.com/avatars-oEn7XKE76IZpyoKB-ANzgWg-t500x500.jpg",
+        website_url="https://particle.fm/",
+        chat_url="https://particle.fm/chat.html",
+        now_playing_url="https://azuracast.particle.fm/api/nowplaying/1",
+        calendar_url="",
+        additional_link_1="",
+        additional_label_1="",
+        additional_link_2="",
+        additional_label_2="",
+        additional_link_3="",
+        additional_label_3="",)
 
     nts1 = RadioStation(
-        name='NTS 1', admin_id=1, stream_url="https://stream-relay-geo.ntslive.net/stream?client=NTSWebApp&t=1669765344306", image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/NTS_Radio_logo.svg/1280px-NTS_Radio_logo.svg.png", website_url="https://www.nts.live", chat_url="https://www.nts.live/chat/1", now_playing_url="https://www.nts.live/api/v2/live")
+        name='NTS 1',
+        admin_id=2,
+        stream_url="https://stream-relay-geo.ntslive.net/stream?client=NTSWebApp&t=1669765344306",
+        image_url="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/NTS_Radio_logo.svg/1280px-NTS_Radio_logo.svg.png",
+        website_url="https://www.nts.live",
+        chat_url="https://www.nts.live/chat/1",
+        now_playing_url="https://www.nts.live/api/v2/live",
+        calendar_url="",
+        additional_link_1="",
+        additional_label_1="",
+        additional_link_2="",
+        additional_label_2="",
+        additional_link_3="",
+        additional_label_3="",
+
+    )
 
     db.session.add(kiosk)
     db.session.add(ifmdisco)
