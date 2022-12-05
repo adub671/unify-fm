39bb7bb822a596e137dc7bc3a3fab0aabda8c90f
ADUB <rootbeer608@gmail.com>
2022-12-04 20:25:21 -0800

    create station conditional render on login

--- react-app/src/components/RadioStation/CreateStationButton.js
+++ react-app/src/components/RadioStation/CreateStationButton.js
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Modal } from "../../context/Modal";
 import CreateStation from "./CreateStation";
 
 function CreateStationButton() {
   const [showModal, setShowModal] = useState(false);
+  const user = useSelector((state) => state.session.user);
   return (
-    <div>
-      <div
-        className="create-station-button"
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        <span> Create A Station</span>
-        <i className="fa-solid fa-plus create-station-icon"></i>
-      </div>
+    <>
+      {user && (
+        <div>
+          <div
+            className="create-station-button"
+            onClick={() => {
+              setShowModal(true);
+            }}
+          >
+            <span> Create A Station</span>
+            <i className="fa-solid fa-plus create-station-icon"></i>
+          </div>
 
-      {showModal && (
-        <Modal
-          onClose={() => {
-            setShowModal(false);
-          }}
-        >
-          <CreateStation setShowModal={setShowModal} />
-        </Modal>
+          {showModal && (
+            <Modal
+              onClose={() => {
+                setShowModal(false);
+              }}
+            >
+              <CreateStation setShowModal={setShowModal} />
+            </Modal>
+          )}
+        </div>
       )}
-    </div>
+    </>
   );
 }
 export default CreateStationButton;
