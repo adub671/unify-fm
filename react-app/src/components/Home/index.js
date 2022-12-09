6e1c4738e72a824a8c67e77169932b99767d1dc4
ADUB <rootbeer608@gmail.com>
2022-12-09 14:59:45 -0800

    carousel styling

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -14,6 +14,7 @@ function Home() {
           </div>
         </div>
         <AllStationsCarousel />
+        <div className="carousel-spacer"></div>
 
         <FavoriteStationsCarousel />
       </div>
